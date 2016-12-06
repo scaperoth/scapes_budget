cf9c174fba43a5a2d5fc3b3062180db57e05d5b2
Prelang Builder <builder@prelang.com>
2016-12-06 21:02:23 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,4 +1,8 @@
 class IncomesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_income, only: [:show, :edit, :update, :destroy]
 
   # GET /incomes

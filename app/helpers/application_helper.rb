89f721f48ed12f3e9859ff57d7239b406ced0192
scaperoth <scaperoth@gmail.com>
2016-12-13 07:06:57 -0500

    finalizing layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
     def active_class(link_path)
         current_page?(link_path) ? 'uk-active' : ''
    end
+
+    def uikit_messages
+        output = ''
+        flash.each do |name, msg|
+            display_class = 'notice'
+            display_class = 'danger' if name == 'alert' || name == 'error'
+            message = content_tag :div, msg, id: "flash_#{name}"
+            close_link = link_to '', '', class: 'uk-alert-close uk-close'
+            output += content_tag :div, close_link + message, class: 'uk-alert uk-alert-' + display_class
+        end
+
+        output.html_safe
+    end
 end
